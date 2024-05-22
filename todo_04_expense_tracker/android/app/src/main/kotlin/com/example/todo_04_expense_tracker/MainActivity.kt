ec934facd227d578b72665b6719dc2aebe2aaef5
Kinzang Dorji <kdorgee2000@gmail.com>
2024-05-22 16:46:04 +0600

    Expense Tracker

---
+++ todo_04_expense_tracker/android/app/src/main/kotlin/com/example/todo_04_expense_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.todo_04_expense_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ todo_04_expense_tracker/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:todo_04_expense_tracker/screens/auth/login_screen.dart';
+import 'package:todo_04_expense_tracker/screens/landing/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
